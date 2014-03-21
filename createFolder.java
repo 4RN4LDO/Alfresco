0fb2450a07dfd74f2dbf858d3525d93956ad3de2
4RN4LDO <arnaldoporras@pernix-solutions.com>
2014-03-21 16:25:12 -0600

    java class
    class parameters

--- createFolder.java
+++ createFolder.java
@@ -4,25 +4,54 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.extensions.webscripts.WebScriptException;
 
+import com.componize.alfresco.repo.node.NodePathResolver;
+
+import org.alfresco.repo.model.Repository;
+import org.alfresco.service.ServiceRegistry;
 import org.alfresco.web.scripts.AbstractWebScript;
 import org.alfresco.web.scripts.WebScriptRequest;
 import org.alfresco.web.scripts.WebScriptResponse;
 
+public class CreateFolder extends AbstractWebScript {
+    private static Logger LOG = Logger.getLogger(NodePropertySvc.class);
 
-public class createFolder extends AbstractWebScript {
-
+    protected Repository m_repository = null;
+    protected ServiceRegistry m_serviceRegistry = null;
+    protected NodePathResolver m_nodePathResolver = null;
 
     @Override
     public void execute(final WebScriptRequest request, final WebScriptResponse response) throws IOException {
 
+        LOG.debug("Start executeImpl()");
+
+        ObjectMapper mapper = new ObjectMapper();
+
         try {
 
-        }catch (Throwable e) {
+
+        } catch (Throwable e) {
             String errorMsg = "Unable to retrieve properties for node ";
             throw new WebScriptException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorMsg, e);
         }
     }
 
+
+    public void setRepository(final Repository repository) {
+
+        m_repository = repository;
+    }
+
+    public void setServiceRegistry(final ServiceRegistry registry) {
+
+        m_serviceRegistry = registry;
+    }
+
+    public final void setNodePathResolver(final NodePathResolver nodePathResolver) {
+
+        m_nodePathResolver = nodePathResolver;
+    }
 }
