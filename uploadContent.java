82030190f8df4cab107a4f09ebcca0d18d7ac2c9
4RN4LDO <arnaldoporras@pernix-solutions.com>
2014-03-28 12:09:10 -0600

    add file metada

--- uploadContent.java
+++ uploadContent.java
@@ -3,18 +3,23 @@ package org.ieee.sa.x1ng.webscripts.node;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.ieee.sa.x1ng.webscripts.util.WebScriptUtil;
 import org.springframework.extensions.webscripts.WebScriptException;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.model.Repository;
 import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.model.FileFolderService;
+import org.alfresco.service.cmr.model.FileInfo;
 import org.alfresco.service.cmr.repository.ContentService;
+import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.web.scripts.AbstractWebScript;
 import org.alfresco.web.scripts.WebScriptRequest;
@@ -34,12 +39,31 @@ public class UploadContent extends AbstractWebScript {
         public void execute(final WebScriptRequest request, final WebScriptResponse response) throws IOException {
 
             LOG.debug("Start executeImpl()");
+            FileFolderService fileService = m_serviceRegistry.getFileFolderService();
+            ContentService contService = m_serviceRegistry.getContentService();
+            NodeService nodeService = m_serviceRegistry.getNodeService();
+           FileFolderService fileFolderService = m_serviceRegistry.getFileFolderService();
+
+            String nodeRefStr = request.getParameter("NodeRef");
+            String uploadName = request.getParameter("Name");
+            NodeRef nodeRef = new NodeRef(nodeRefStr);
 
             try {
+                FileInfo info;
+                if (uploadName != null && !uploadName.isEmpty()) {
+                    info = fileService.create(nodeRef, uploadName, ContentModel.TYPE_CONTENT);
+                    Map<QName,Serializable> props = new HashMap<QName,Serializable>();
+                    props.put(ContentModel.PROP_NAME, uploadName);
+                    props.put(ContentModel.PROP_TITLE, request.getParameter("Title"));
+                    props.put(ContentModel.PROP_DESCRIPTION, request.getParameter("Description"));
+                    props.put(ContentModel.PROP_AUTHOR, request.getParameter("Author"));
+                    nodeService.setProperties(info.getNodeRef(), props);
+                    ContentWriter writer = m_serviceRegistry.getFileFolderService().getWriter(info.getNodeRef());
+                    writer.setLocale(Locale.ENGLISH);
 
-               
+                }
             } catch (Throwable e) {
-                String errorMsg = "Unable to save content to node " + nodeRefStr.toString();
+                String errorMsg = "Unable to save content to node ";
                 throw new WebScriptException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorMsg, e);
             }
 
