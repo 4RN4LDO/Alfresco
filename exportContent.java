aad5ea2304aa6000d39ccbb32a1b28d83dd4e558
4RN4LDO <arnaldoporras@pernix-solutions.com>
2014-03-31 13:34:27 -0600

    Content reader

--- exportContent.java
+++ exportContent.java
@@ -1,24 +1,32 @@
 package org.ieee.sa.x1ng.webscripts.node;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.ieee.sa.x1ng.webscripts.util.WebScriptUtil;
+import org.springframework.extensions.webscripts.WebScriptException;
 
 import com.componize.alfresco.repo.node.NodePathResolver;
 
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.repo.model.Repository;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.model.FileFolderService;
+import org.alfresco.service.cmr.repository.ContentReader;
+import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.web.scripts.AbstractWebScript;
 import org.alfresco.web.scripts.WebScriptRequest;
 import org.alfresco.web.scripts.WebScriptResponse;
 
-public class ExportContent extends AbstractWebScript{
-
-    private static Logger LOG = Logger.getLogger(NodePropertySvc.class);
+public class ExportContent extends AbstractWebScript {
+    private static Logger LOG = Logger.getLogger(ExportContent.class);
 
     protected Repository m_repository = null;
     protected ServiceRegistry m_serviceRegistry = null;
@@ -26,16 +34,48 @@ public class ExportContent extends AbstractWebScript{
 
     @Override
     public void execute(final WebScriptRequest request, final WebScriptResponse response) throws IOException {
+
         LOG.debug("Start executeImpl()");
 
         FileFolderService fileService = m_serviceRegistry.getFileFolderService();
+        ContentService contService = m_serviceRegistry.getContentService();
 
         ObjectMapper mapper = new ObjectMapper();
 
         String nodeRefStr = WebScriptUtil.getNodeRef(request);
         NodeRef nodeRef = new NodeRef(nodeRefStr);
 
+        try {
+            if (nodeRef != null) {
+                ContentReader reader = contService.getReader(nodeRef, ContentModel.PROP_CONTENT);
+                File file = new File("C:/Users/Administrator/Downloads/");
+                reader.getContent(file);
 
+            }
+
+
+            response.getWriter().write(mapper.writeValueAsString(nodeRef));
+            response.setContentType(MimetypeMap.MIMETYPE_JSON);
+            response.setContentEncoding(StandardCharsets.UTF_8.name());
+        } catch (Throwable e) {
+            String errorMsg = "Unable to retrieve properties for node ";
+            throw new WebScriptException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorMsg, e);
+        }
     }
 
+
+    public void setRepository(final Repository repository) {
+
+        m_repository = repository;
+    }
+
+    public void setServiceRegistry(final ServiceRegistry registry) {
+
+        m_serviceRegistry = registry;
+    }
+
+    public final void setNodePathResolver(final NodePathResolver nodePathResolver) {
+
+        m_nodePathResolver = nodePathResolver;
+    }
 }
